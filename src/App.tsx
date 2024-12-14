3f5b3e5c850d6a325efdea9cca27a13e09f0fc17
Linkinlog <dahlton@dahlton.org>
2024-12-14 08:43:20 -0500

    feat: waitlist

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>forever</h1>
+      <a href="https://getwaitlist.com/waitlist/23093" target="_blank" rel="noopener noreferrer" className="waitlist-link">
+        Join our Waitlist
+      </a>
+    </>
+  )
+}
+
+export default App
